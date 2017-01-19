e9d69e0098bc388b3a0fee8267c3f1510ea75474
Liam Reid <liam.reid@enflick.com>
2017-01-19 16:56:30 -0500

    created schedule and timeline

---
+++ app/assets/javascripts/component_constants/events.js
@@ -0,0 +1,30 @@
+const Events = [
+    {
+        title: 'Welcome Drinks',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non libero euismod, cursus sapien non, tristique nisl. Nunc arcu velit, aliquam id gravida eu, feugiat id odio. Vestibulum mattis eros diam, vel vestibulum odio euismod a. Duis ligula lectus, blandit ut sem id, luctus congue libero. Aenean auctor nulla a.',
+        startTime: '5:00pm',
+        endTime: '6:00pm',
+        image: 'http://placehold.it/150x150'
+    },
+    {
+        title: 'Ceremony',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non libero euismod, cursus sapien non, tristique nisl. Nunc arcu velit, aliquam id gravida eu, feugiat id odio. Vestibulum mattis eros diam, vel vestibulum odio euismod a. Duis ligula lectus, blandit ut sem id, luctus congue libero. Aenean auctor nulla a.',
+        startTime: '6:00pm',
+        endTime: '6:30pm',
+        image: 'http://placehold.it/150x150'
+    },
+    {
+        title: 'Reception',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non libero euismod, cursus sapien non, tristique nisl. Nunc arcu velit, aliquam id gravida eu, feugiat id odio. Vestibulum mattis eros diam, vel vestibulum odio euismod a. Duis ligula lectus, blandit ut sem id, luctus congue libero. Aenean auctor nulla a.',
+        startTime: '6:30pm',
+        endTime: '11:00pm',
+        image: 'http://placehold.it/150x150'
+    },
+    {
+        title: 'Wrap-up',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non libero euismod, cursus sapien non, tristique nisl. Nunc arcu velit, aliquam id gravida eu, feugiat id odio. Vestibulum mattis eros diam, vel vestibulum odio euismod a. Duis ligula lectus, blandit ut sem id, luctus congue libero. Aenean auctor nulla a.',
+        startTime: '11:00pm',
+        endTime: '1:00am',
+        image: 'http://placehold.it/150x150'
+    }
+];
--- app/assets/javascripts/components/schedule.js.jsx
+++ app/assets/javascripts/components/schedule.js.jsx
@@ -1,7 +1,62 @@
-var Schedule = React.createClass({
-    render: function(){
+var Event = React.createClass({
+    render: function () {
         return (
-            <div id="schedule">Schedule</div>
+            <div className="event row">
+                <div className="event-image flex-center col-xs-2">
+                    <img src={this.props.event.image}></img>
+                </div>
+                <div className="event-description flex-center col-xs-8">
+                    <h4>{this.props.event.title}</h4>
+                    <p>{this.props.event.description}</p>
+                </div>
+                <div className="event-info flex-center col-xs-2">
+                    Time: {this.props.event.startTime} - {this.props.event.endTime}
+                </div>
+            </div>
+        );
+    }
+});
+
+var Timeline = React.createClass({
+    render: function () {
+        let events = Events.map(function (event, index) {
+            var icon = (index === this.props.selectedEvent) ? 'fa-circle' : 'fa-circle-thin';
+            return (
+                <div key={index} className="timeline-item flex-center" onClick={this.props.changeSelected.bind(null, index)}>
+                    <span><i className={"fa " + icon}></i></span>
+                    {event.title}<br/>
+                    {event.startTime}
+                </div>
+            );
+        }.bind(this));
+        return (
+            <div id="timeline">
+                {events}
+            </div>
+        );
+    }
+});
+
+var Schedule = React.createClass({
+    getInitialState: function () {
+        return ({
+            selected_event: 0
+        });
+    },
+
+    changeSelected: function (selected) {
+        this.setState({selected_event: selected});
+    },
+
+    render: function () {
+        return (
+            <div id="schedule" className="container">
+                <div className="row">
+                    <h2>Schedule</h2>
+                </div>
+                <Event event={Events[this.state.selected_event]}/>
+                <Timeline selectedEvent={this.state.selected_event} changeSelected={this.changeSelected}/>
+            </div>
         );
     }
 });
