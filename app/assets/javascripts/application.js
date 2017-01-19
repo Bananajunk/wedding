aacbf9e3ce68dcbb7719231729ea9b611d3f8a0e
Liam Reid <liam.reid@enflick.com>
2017-01-19 17:54:42 -0500

    load bootstrap after jquery; added schema file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require react
 //= require react_ujs
 //= require components
+//= require bootstrap
 //= require_tree .
