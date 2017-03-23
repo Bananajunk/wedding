3066448aca85b26961fbd4c23d2bb92397877099
Liam Reid <liam.reid@enflick.com>
2017-03-23 18:46:14 -0400

    cleaned up thank you display

--- app/assets/javascripts/components/rsvp/thanks.js.jsx
+++ app/assets/javascripts/components/rsvp/thanks.js.jsx
@@ -1,42 +1,18 @@
 var Thanks = React.createClass({
-    determineGuest: function () {
+    determineInvite: function () {
+        var result = this.props.invite.name;
         if (this.props.invite.guest && this.props.invite.guest_name != null) {
-            return (
-                <div id="guest">
-                    <p>&</p>
-                    <h4>{this.props.invite.guest_name}</h4>
-                </div>
-            );
-        } else {
-            return null;
+            return result + " & " + this.props.invite.guest_name;
         }
-    },
-    determineChildren: function () {
-        if (this.props.invite.children.length > 0) {
-            var children = this.props.invite.children.map(function(child){
-                return child.name;
-            });
-            var child_list = children.join();
-            return (
-                <div id="children">
-                    <p>&</p>
-                    <h4>your children</h4>
-                    <p>({child_list})</p>
-                </div>
-            );
-        } else {
-            return null;
-        }
+
+        return result;
     },
     render: function () {
-        var guest = this.determineGuest();
-        var children = this.determineChildren();
+        var thank_you = this.determineInvite();
         return (
             <div>
                 <h4>Thanks for your RSVP!</h4>
-                <h4>{this.props.invite.name}</h4>
-                {guest}
-                {children}
+                <h2>{thank_you}</h2>
             </div>
         )
     }
