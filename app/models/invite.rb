fc41c70d572e247eea40354c14b864c06b2a5fbb
Liam Reid <liam.reid@enflick.com>
2017-03-26 19:47:17 -0400

    removed required presence of guest boolean

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,14 +1,8 @@
 class Invite < ApplicationRecord
   has_many :children
 
-  validates :name, :guest, :code, presence: true
+  validates :name, :code, presence: true
   validates :code, uniqueness: true
-
-  before_validation :generate_code
-
-  def generate_code
-    self.code = '%04d' % rand(10**4) if self.code.nil?
-  end
 
   def children?
     !self.children.empty?
