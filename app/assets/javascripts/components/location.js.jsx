a3c12153ccbf4b0fe1f9f7e19b0eb24455914109
Liam Reid <liam.reid@enflick.com>
2017-03-27 18:30:03 -0400

    layout adjustments, cross browser updates

--- app/assets/javascripts/components/location.js.jsx
+++ app/assets/javascripts/components/location.js.jsx
@@ -39,18 +39,20 @@ var Location = React.createClass({
     render: function () {
         return (
             <div id="location" className="container section">
-                <div className="location-info col-md-6 col-sm-12">
-                    <h2>Alton Mill Arts Centre</h2>
-                    <p className="lead">
-                        1402 Queen St. <br/>
-                        Village of Alton <br/>
-                        Caledon, Ontario <br/>
-                        Canada, L7K 0C3 <br/>
-                    </p>
-                    <LocationThumbnails setSpotlight={this.setSpotlight} />
-                </div>
-                <div className="col-md-6 col-sm-12">
-                    <LocationSpotlight image={this.state.image}/>
+                <div className="row">
+                    <div className="location-info col-md-6 col-sm-12">
+                        <h2>Alton Mill Arts Centre</h2>
+                        <p className="lead">
+                            1402 Queen St. <br/>
+                            Village of Alton <br/>
+                            Caledon, Ontario <br/>
+                            Canada, L7K 0C3 <br/>
+                        </p>
+                        <LocationThumbnails setSpotlight={this.setSpotlight} />
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                        <LocationSpotlight image={this.state.image}/>
+                    </div>
                 </div>
             </div>
         );
